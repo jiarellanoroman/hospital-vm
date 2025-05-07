e63ad6137b17f66f7099150a18350d05b716ca52
Rmindboy <l.toledo@duocuc.cl>
2025-05-07 16:45:40 -0400

    hola mundo lusiano

--- hospital-vm/src/main/java/com/hospital_vm/cl/hospital_vm/HospitalVmApplication.java
+++ hospital-vm/src/main/java/com/hospital_vm/cl/hospital_vm/HospitalVmApplication.java
@@ -8,6 +8,7 @@ public class HospitalVmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalVmApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
